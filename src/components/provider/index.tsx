749f8687d561c615fb89b94bb74ad48f1bca2e24
SonPH63195 <sonpham3798@gmail.com>
2025-07-20 13:29:24 +0700

    refactor: Consolidate ThemeProvider within main Provider component

--- src/components/provider/index.tsx
+++ src/components/provider/index.tsx
@@ -1,10 +1,16 @@
 import { Toaster } from "sonner";
+import { ThemeProvider } from "./theme-provider";
 
 export default function Provider({ children }: { children: React.ReactNode }) {
 	return (
-		<>
+		<ThemeProvider
+			attribute="class"
+			defaultTheme="system"
+			enableSystem
+			disableTransitionOnChange
+		>
 			{children}
 			<Toaster />
-		</>
+		</ThemeProvider>
 	);
 }
